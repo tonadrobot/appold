9d54798efde6d91724c6b681b42767f9b3a35e30
TON Ad Admin <tonadrobot@proton.me>
2024-11-30 17:52:14 +0100

    update bot in app

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ class App {
                 }
 
                 app.data = data;
-                $("#refLink").html("t.me/TonAdRobot/miner?startapp=" + data.code);
+                $("#refLink").html("t.me/TonCityRobot/miner?startapp=" + data.code);
                 $("#earnings").html(data.earnings);
                 $("#tmu").html(data.tmu.toFixed(9));
                 app.tmu = data.tmu;
